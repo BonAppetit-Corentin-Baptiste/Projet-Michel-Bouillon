b29732945d03ebf2762f95d96f7eeaaad355ff60
Marchand <baptistemarchand2007@gmail.com>
2026-08-29 23:20:34 +0200

    Modification du design mobil et desktop

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,10 +7,5 @@ menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   navBar.classList.toggle("open");
   overlay.classList.toggle("active");
-  
-  if (navLinks.classList.contains("open")) {
-    menuBtn.src = "./assets/img/icon-close.svg";
-  } else {
-    menuBtn.src = "./assets/img/menu-burger.png";
-  }
+  menuBtn.classList.toggle("open");
 });
